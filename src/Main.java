6086e6a8fc61c6209ecd8b45ffeb937725a90cd2
Andrzej Pawlowski <panandy@poczta.onet.pl>
2019-10-17 11:51:43 +0200

    program prawidłowo liczy

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,17 @@ public class Main {
 
     public static void main(String[] args) {
 
+    int i =1;
+    int sum =0;
 
+
+        for (i = 1; i <= 100 ; i++) {
+
+        if (i%2 != 0) {
+            sum = sum + i;
+        }
+
+        }
+        System.out.println("Suma liczb nieparzystych z przedziału 1 - 100 wynoszą: " + sum);
     }
 }
